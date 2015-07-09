7e12d7fb4ccc6b03d016c38ed5df448a78f110cc
ioc-dam-m05 <jserret3@ioc.cat>
2015-07-09 16:47:17 +0200

    primera versio

---
+++ src/comptadors/ComptadorBasic.java
@@ -0,0 +1,48 @@
+package comptadors;
+
+/**
+ * @author JRamon
+ *
+ */
+public class ComptadorBasic {
+	private int compte;
+
+	/**Crea un comptador inicialitzat a zero
+	 * 
+	 */
+	public ComptadorBasic() {
+
+	}
+	
+	/**Crea un comptador inicialitzat a compt
+	 * @param compte valor inicial del comptador
+	 */
+	public ComptadorBasic(int compte) {
+		this.compte = compte;
+	}
+
+	/** consulta el valor del comptador
+	 * @return the compt
+	 */
+	public int getCompte() {
+		return compte;
+	}
+
+	/** modifica el valor del comptador
+	 * @param compt the compt to set
+	 */
+	public void setCompte(int compt) {
+		this.compte = compt;
+	}
+	
+	
+	
+	/**
+	 * incrementa en una unitat el comptador
+	 */
+	public void incrementa(){
+		this.compte++;
+	}
+	
+
+}
---
+++ src/comptadors/ProvaExemple.java
@@ -0,0 +1,14 @@
+package comptadors;
+
+public class ProvaExemple {
+
+	public static void main(String[] args) {
+		ComptadorBasic c1=new ComptadorBasic(), c2=new ComptadorBasic(3);
+		
+		c1.incrementa();
+		c2.incrementa();
+		System.out.println("c1:"+c1.getCompte());
+		System.out.println("c2:"+c2.getCompte());
+	}
+
+}
